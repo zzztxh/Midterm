e1306434ba50f6bbd263ee91e1956cd569c42ba0
TZ <TZ@10.180.62.145>
2017-10-17 14:52:30 -0400

    Add major in Course, and import regex in PersonExpection

--- Midterm Coding Project/src/main/java/com/cisc181/core/Course.java
+++ Midterm Coding Project/src/main/java/com/cisc181/core/Course.java
@@ -2,11 +2,14 @@ package com.cisc181.core;
 
 import java.util.UUID;
 
+import com.cisc181.eNums.eMajor;
+
 public class Course {
 
 	private UUID CourseID;
 	private String CourseName;
 	private int GradePoints;
+	private eMajor Major;
 	
 	public Course(UUID CourseID, String CourseName, int GradePoints){
 		this.CourseID = CourseID;
@@ -31,6 +34,12 @@ public class Course {
 	}
 	public void setGradePoints(int gradePoints) {
 		GradePoints = gradePoints;
+	}
+	public eMajor getMajor() {
+		return Major;
+	}
+	public void setMajor(eMajor major) {
+		Major = major;
 	}
 	
 
